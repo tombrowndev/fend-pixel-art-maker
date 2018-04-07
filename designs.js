88dffdc060f10daf6edd21ae9ed601c3b94c1dc5
Tom Brown <tombrown91@googlemail.com>
2018-04-07 14:31:26 +0100

    Initial Commit 2

--- designs.js
+++ designs.js
@@ -1,10 +1,115 @@
-// Select color input
-// Select size input
+// GLobal
+const sizeForm = document.getElementById('sizePicker');
+const canvas = document.getElementById('pixelCanvas');
+let penIsActive = eraserIsActive = false;
 
-// When size is submitted by the user, call makeGrid()
+// Make the grid
+function makeGrid(event) {
 
-function makeGrid() {
+	// Prevent the form from submitting
+	event.preventDefault();
 
-// Your code goes here!
+	// Remove existing elemernts from the cavas table
+	while(canvas.firstChild) {
+		canvas.removeChild(canvas.firstChild);
+	}
+
+	// Get the width and height height values for making the grid
+	let gridHeight = document.getElementById('inputHeight').value;
+	let gridWidth = document.getElementById('inputWidth').value;
+
+	// Reset the form
+	this.reset();
+
+	// Loop through rows
+	for(let i = 0; i < gridHeight; i++) {
+
+		// Create a row
+		let row = document.createElement('tr');
+
+		// Loop through columns
+		for(let j = 0; j < gridWidth; j++) {
+
+			// Create a column
+			let column = document.createElement('td');
+
+			// Append columns to the row
+			row.appendChild(column);
+
+		}
+
+		// Append rows to the canvas table
+		canvas.appendChild(row);
+
+	}
 
 }
+
+// Paint to the canvas
+function draw(e) {
+
+	// Prevent default behaviour
+	e.preventDefault();
+
+	// Check if target is a cell
+	if(e.target.nodeName == 'TD' && (penIsActive || eraserIsActive)) {
+
+		console.log('drawing');
+
+		let color;
+
+		if(penIsActive) {
+
+			// Read the color from the picker
+			color = document.getElementById('colorPicker').value;
+
+		} else if (eraserIsActive) {
+
+			// Set color to null for eraser
+			color = null;
+
+		}
+
+		// Apply color to cell background
+		e.target.style.backgroundColor = color;
+
+	}
+
+}
+
+function enableDrawing(e) {
+
+	// Active the pen
+	if(e.button === 0) penIsActive = true;
+
+	// Activate the eraser
+	if(e.button === 2) eraserIsActive = true;
+
+}
+
+function disableDrawing() {
+
+	penIsActive = eraserIsActive = false;
+
+}
+
+// Submit grid maker form
+sizePicker.addEventListener('submit', makeGrid);
+
+// Enables pen or erasor
+canvas.addEventListener('mousedown', enableDrawing);
+
+// Disables pen and eraser if buttons are released
+document.addEventListener('mouseup', disableDrawing);
+
+// Disables pen and eraser if ouse leaves the canvas
+canvas.addEventListener('mouseleave', disableDrawing);
+
+// Paint or Erase to canvas (click)
+canvas.addEventListener('mousedown', draw);
+
+// Paint or Erase to canvas (drag)
+canvas.addEventListener('mouseover', draw);
+
+// Prevent context menu in canvas
+canvas.addEventListener('contextmenu', event => event.preventDefault());
